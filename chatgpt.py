d9e7ba7f69d8ab08aa63a2db935dc98a95ca595b
shanleynguyen15 <shminguy@ucsc.edu>
2024-01-20 11:14:27 -0800

    add chatGPTfolder

---
+++ chatgpt.py
@@ -0,0 +1,51 @@
+from openai import OpenAI
+
+client = OpenAI()
+
+# Once you add your API key below, make sure to not share it with anyone! The API key should remain private.
+OPENAI_API_KEY= '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'
+
+# completion = client.chat.completions.create(
+#   model="gpt-3.5-turbo",
+messages=[
+  {"role": "system", "content": "You are a intelligent assistant."}]
+#     # {"role": "user", "content": "You are a garbage disposer."}
+#   ]
+# )
+#
+# print(completion.choices[0].message)
+
+
+
+item_description = "banana peel"
+question1 = "Is a " + item_description + " compostable, recyclable, or disposable? (one word answer)"
+print(question1)
+message = question1
+if message:
+  messages.append(
+    {"role": "user", "content": message},
+  )
+  completion = client.chat.completions.create(model="gpt-3.5-turbo", messages=messages)
+response1 = completion.choices[0].message.content
+print(f"{response1}")
+edit_response1 = response1.replace(".", "")
+
+question2 = "What to do with " + edit_response1 + "s? (summary)"
+print(question2)
+message2 = question2
+if message2:
+  messages.append(
+    {"role": "user", "content": message2},
+  )
+  completion = client.chat.completions.create(model="gpt-3.5-turbo", messages=messages)
+response2 = completion.choices[0].message.content
+print(f"{response2}")
+
+
+
+
+
+
+
+
+
