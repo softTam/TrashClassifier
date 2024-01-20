f4df71f7f039ca96aaefea49483b2e5d161c7948
Trien Bang Huynh <trienbanghuynh4701@gmail.com>
2024-01-20 13:27:28 -0800

    add Flask

---
+++ Flask/detect.py
@@ -0,0 +1,22 @@
+import os
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]= "/Users/banghuynh/trash-app/norse-lens-411805-d1b767e58bfa.json"
+def localize_objects(path):
+    """Localize objects in the local image.
+    Args:
+    path: The path to the local file.
+    """
+    from google.cloud import vision
+
+    client = vision.ImageAnnotatorClient()
+
+    with open(path, "rb") as image_file:
+        content = image_file.read()
+    image = vision.Image(content=content)
+
+    objects = client.object_localization(image=image).localized_object_annotations
+
+    return objects[0].name
+
+if __name__ == '__main__':
+    print(localize_objects('./banana.jpeg'))
---
+++ Flask/main.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, jsonify
+from detect import localize_objects
+import os
+from werkzeug.utils import secure_filename
+app = Flask(__name__)
+
+
+UPLOAD_FOLDER="/Users/banghuynh/trash-app/images"
+
+ALLOWED_EXTENSIONS = set(['txt','pdf','png', 'jpg','jpeg','gif'])
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.',1)[1].lower() in ALLOWED_EXTENSIONS
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+@app.route('/media/upload', methods = ['POST'])
+def upload_media():
+    if 'file' not in request.files:
+        return jsonify({'error':'media not provided'}), 400
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({'error':'no file selected'}), 400
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    return jsonify({'msg':'media uploaded successfully'})
+
+
+    
+@app.route('/', methods = ["GET"])
+def hello():
+    url = request.args.get("url_image")
+    return localize_objects(url)
+    # return "Hello"
+    
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
